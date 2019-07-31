dc78405049d05936b375b4dab36b0f4d93631f51
Wesley Berry <wesleyberry52@gmail.com>
2019-07-31 17:59:38 -0500

    Xs and Os alternate

--- src/index.js
+++ src/index.js
@@ -2,25 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-      return (
-        <button 
-            className="square" 
-            onClick={() => this.props.onClick()}
-        >
-          {this.props.value}
-        </button>
+  function Square(props) {
+      return(
+          <button className="square" onClick={props.onClick}>
+              {props.value}
+          </button>
       );
-    }
   }
   
   class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            squares: Array(9).fill(null)
+            squares: Array(9).fill(null),
+            xIsNext: true
         };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext
+        });
     }
 
     renderSquare(i) {
